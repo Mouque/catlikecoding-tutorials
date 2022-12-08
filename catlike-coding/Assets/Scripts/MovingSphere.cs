c317778b7fa5170c0994908c2fbaa5c845bdda13
Henrique Moura <henriq8383@gmail.com>
2022-12-08 19:00:07 -0300

    Update MovingSphere.cs

--- catlike-coding/Assets/Scripts/MovingSphere.cs
+++ catlike-coding/Assets/Scripts/MovingSphere.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    
+    void Update() 
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y);
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+    }
 }
