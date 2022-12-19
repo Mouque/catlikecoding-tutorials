d2d5c8f7040e866109289a476a6aef22213f4b81
Henrique Moura <henriq8383@gmail.com>
2022-12-19 18:26:05 -0300

    Physics tutorial até no wall jumping

--- catlike-coding/Assets/Scripts/MovingSphere.cs
+++ catlike-coding/Assets/Scripts/MovingSphere.cs
@@ -9,50 +9,76 @@ public class MovingSphere : MonoBehaviour
 
     [SerializeField, Range(0f, 100f)]
     float maxAcceleration = 10f;
-    Vector3 velocity;
+    Vector3 velocity, desiredVelocity;
 
-    [SerializeField]
-    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+    Rigidbody body;
 
-    [SerializeField, Range(0f, 1f)]
-    float bounciness = 0.5f;
+    bool desiredJump;
+    bool onGround;
+
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-        Vector3 desiredVelocity =
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        desiredVelocity =
             new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+        desiredJump |= Input.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        velocity = body.velocity;
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x =
             Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z =
             Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        Vector3 displacement = velocity * Time.deltaTime;
-        Vector3 newPosition = transform.localPosition + displacement;
+        body.velocity = velocity;
 
-        if (newPosition.x < allowedArea.xMin)
+        if (desiredJump)
         {
-            newPosition.x = allowedArea.xMin;
-            velocity.x = -velocity.x * bounciness;
+            desiredJump = false;
+            Jump();
         }
-        else if (newPosition.x > allowedArea.xMax)
-        {
-            newPosition.x = allowedArea.xMax;
-            velocity.x = -velocity.x * bounciness;
-        }
-        if (newPosition.z < allowedArea.yMin)
-        {
-            newPosition.z = allowedArea.yMin;
-            velocity.z = -velocity.z * bounciness;
-        }
-        else if (newPosition.z > allowedArea.yMax)
-        {
-            newPosition.z = allowedArea.yMax;
-            velocity.z = -velocity.z * bounciness;
-        }
-        transform.localPosition = newPosition;
-        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        body.velocity = velocity;
+        onGround = false;
+    }
 
+    void Jump()
+    {
+        if (onGround)
+        {
+            velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void EvaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            // Debug.Log(normal);
+            onGround |= normal.y >= 0.9f;
+        }
     }
 }
